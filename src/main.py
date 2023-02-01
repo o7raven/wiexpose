80c692e23004966fb76733298f4bda2e930ddad4
Raven <yakubsiq@gmail.com>
2023-02-01 19:29:24 +0100

    The first version

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
 import subprocess
 
-data = subprocess.run('netsh wlan show profile', shell=True, capture_output = True).stdout.decode('utf-8').split('\n')
+data = subprocess.run('netsh wlan show profiles', shell=True, capture_output = True).stdout.decode('utf-8').split('\n')
 
 profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
 
-for i in profiles:
-    results = subprocess.run(f'netsh wlan show profile {i} key=clear', shell=True, capture_output=True).stdout.decode('utf-8').split('\n')
-    results = [b.split(':')[1][1:-1] for b in results if "Key Content" in b]
+for profile in profiles:
+    profileInfo = subprocess.run(f'netsh wlan show profile {profile} key=clear', shell=True, capture_output=True).stdout.decode('utf-8').split('\n')
+    pwd = [i.split(':')[1][1:-1] for i in profileInfo if "Key Content" in i]
 
 
     try:
-        print (f"{i}| {results[0]}")
+        print (f"{profile} -> {pwd[0]}")
     except IndexError:
-        print (f"{i}|  ' ' ")
+        print (f"{profile}|  ' ' ")
